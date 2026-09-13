fa036a5ec4855cd472b5ae778e8831274e78c95f
Siddanagouda Patil <64456315+siddanagoudampatil@users.noreply.github.com>
2026-09-13 16:37:51 -0700

    Add: design-twitter-feed - submission-6

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-6.js
@@ -0,0 +1,73 @@
+class Twitter {
+    constructor() {
+        this.followMap = {};
+        this.tweetMap = {};
+        this.time = 0;
+    }
+
+    /**
+     * @param {number} userId
+     * @param {number} tweetId
+     * @return {void}
+     */
+    postTweet(userId, tweetId) {
+        if (!this.tweetMap[userId]) {
+            this.tweetMap[userId] = [];
+        }
+        this.tweetMap[userId].push([tweetId, this.time++]);
+    }
+
+    /**
+     * @param {number} userId
+     * @return {number[]}
+     */
+    getNewsFeed(userId) {
+        const followeeList = [userId];
+        followeeList.push(...Array.from(this.followMap[userId] || new Set()));
+        const heap = new MinPriorityQueue((x) => x[1]);
+
+        for (const followeeId of followeeList) {
+            if (this.tweetMap[followeeId]) {
+                for (const tweet of this.tweetMap[followeeId]) {
+                    heap.enqueue(tweet);
+
+                    if (heap.size() > 10) {
+                        heap.pop();
+                    }
+                }
+            }
+        }
+
+        const feeds = [];
+        console.log(userId)
+        while (heap.size() > 0) {
+            // console.log(heap.dequeue())
+            feeds.push(heap.dequeue()[0]);
+        }
+
+        return feeds.reverse();
+    }
+
+    /**
+     * @param {number} followerId
+     * @param {number} followeeId
+     * @return {void}
+     */
+    follow(followerId, followeeId) {
+        if (!this.followMap[followerId]) {
+            this.followMap[followerId] = new Set();
+        }
+        this.followMap[followerId].add(followeeId);
+    }
+
+    /**
+     * @param {number} followerId
+     * @param {number} followeeId
+     * @return {void}
+     */
+    unfollow(followerId, followeeId) {
+        if (this.followMap[followerId] && this.followMap[followerId].has(followeeId)) {
+            this.followMap[followerId].delete(followeeId)
+        }
+    }
+}
