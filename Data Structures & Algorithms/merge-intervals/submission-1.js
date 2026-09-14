88bd3b71297fee5574f72c8e2029b64383d21749
Siddanagouda Patil <64456315+siddanagoudampatil@users.noreply.github.com>
2026-09-14 14:40:15 -0700

    Add: merge-intervals - submission-1

---
+++ Data Structures & Algorithms/merge-intervals/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        intervals.sort((a, b) => a[0] - b[0]);
+        const res = [];
+        let j = 0;
+
+        while (j < intervals.length) {
+            const newInterval = intervals[j];
+            while (j < intervals.length && newInterval[1] >= intervals[j][0]) {
+                newInterval[0] = Math.min(newInterval[0], intervals[j][0]);
+                newInterval[1] = Math.max(newInterval[1], intervals[j][1]);
+                j++;
+            }
+            res.push(newInterval);
+        }
+
+        return res;
+    }
+}
