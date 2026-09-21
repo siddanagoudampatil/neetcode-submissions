493fd221c4ca1eeb503fcbcc3ced589e66609ff6
Siddanagouda Patil <64456315+siddanagoudampatil@users.noreply.github.com>
2026-09-21 12:47:43 -0700

    Add: meeting-schedule - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule/submission-6.js
@@ -0,0 +1,33 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals) {
+        const n = intervals.length;
+        if (n <= 1) {
+            return true;
+        }
+        intervals.sort((a, b) => a.start - b.start);
+
+        let i = 0;
+        while (i < n) {
+            if (i + 1 < n && intervals[i].end > intervals[i + 1].start) {
+                return false;
+            }
+            i++;
+        }
+
+        return true;
+    }
+}
