43655791046995d0501f0daca7b6b8cbfac6e5f1
Siddanagouda Patil <64456315+siddanagoudampatil@users.noreply.github.com>
2026-09-23 13:36:56 -0700

    Add: meeting-schedule-ii - submission-8

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-8.js
@@ -0,0 +1,35 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {number}
+     */
+    minMeetingRooms(intervals) {
+        const time = [];
+
+        for (const interval of intervals) {
+            time.push([interval.start, 1]);
+            time.push([interval.end, -1]);
+        }
+
+        time.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]);
+
+        let res = 0, count = 0;
+
+        for (const t of time) {
+            count += t[1];
+            res = Math.max(res, count);
+        }
+
+        return res;
+    }
+}
